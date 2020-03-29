dc628d437b1e68e8189a1fadb5795ee7291b2e93
G.F.M-M <gonzalofidalgo@MacBook-Pro-de-Gonzalo.local>
2020-03-29 21:13:44 +0200

    Initial commit 💪🏻

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import * as express from "express";
+const app = express();
+
+
+// No poner aquí las rutas a pelo, debemos importar el fichero index.ts del directorio routes, donde se cargarán todas las rutas de la API 
+app.get("/", (req, res) => {
+    console.log('Hello World')
+    res.send("Hello World")
+})
+
+app.get("/:name", (req, res) => {
+    let name: string = req.param("name")
+    console.log(`Hello ${name}`)
+    res.send(`Hello ${name}`)
+})
+
+// Sustituir este PORT por una llamada al servidor de configuración que le diga en que puerto debe levantar el servidor 
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running in http://localhost:${PORT}`)
+})
